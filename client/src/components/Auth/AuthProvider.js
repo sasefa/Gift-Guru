3fc0c1bcaf2c74e773140b54563f027c00bd35ef
sasefa <131699944+sasefa@users.noreply.github.com>
2023-10-16 17:05:32 -0400

    Update AuthProvider.js

--- client/src/components/Auth/AuthProvider.js
+++ client/src/components/Auth/AuthProvider.js
@@ -1,16 +1,11 @@
 import React, { createContext, useState, useEffect } from 'react';
 
-// Create context
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  // State
   const [currentUser, setCurrentUser] = useState(null);
 
-  // On mount & will unmount
   useEffect(() => {
-    // Your Login/Register/Logout Functions Here
-    // This will keep track of User State
   }, [])
 
   return (
