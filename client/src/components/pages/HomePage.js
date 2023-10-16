11c5d8a3602e37a59a66dababccd2bf079c87907
sasefa <131699944+sasefa@users.noreply.github.com>
2023-10-16 13:10:44 -0400

    Add files via upload

---
+++ client/src/components/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to GiftGenius!</h1>
+      <p>Your one-stop solution for all your gifting needs.</p>
+    </div>
+  );
+}
+
+export default HomePage;
