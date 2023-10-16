9de6859ac8ba62bcf3832887b1b0a5e1adddb5d9
sasefa <131699944+sasefa@users.noreply.github.com>
2023-10-16 17:18:14 -0400

    Update router.js

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,4 @@
 function Router({children}) {
-    // This would be where you would set up and handle 
-    // your app's routing system
     <Route path="/questionnaire">
     <Questionnaire />
 </Route>
