85c9fee3ce1970f4870bb2fd1f09aa32373b7ef9
sasefa <131699944+sasefa@users.noreply.github.com>
2023-10-17 20:32:23 -0400

    Delete client directory

--- client/src/components/pages/GiftListPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function GiftListPage() {
-  // TODO: Display a list of gifts
-  return <h1>Gift List Page</h1>;
-}
-
-export default GiftListPage;
--- client/src/components/pages/RecommendationsPage.js
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-function RecommendationsPage() {
-  // TODO: Return recommendations based on user inputs
-  return <h1>Recommendations Page</h1>;
-}
-
-export default RecommendationsPage;
